f45a143512fbea5fcf76ba226f680b13d35de8a5
= <=>
2025-04-09 16:40:09 +0200

    Updated values

--- tests/test_wrapping_big_numbers.py
+++ tests/test_wrapping_big_numbers.py
@@ -1,18 +1,27 @@
 import numpy as np
 from npbuffer import NumpyCircularBuffer
 
-b = NumpyCircularBuffer(100000,np.float32)
+b = NumpyCircularBuffer(20000,np.float32)
 
-b.push(np.ones(60000))
-assert b.get_length() == 60000
+b.push(np.zeros(10050))
+assert b.get_length() == 10050
+assert b._head == 0
+assert b._tail == 10050
 
-popped_data1 = b.pop(50000)
-assert b.get_length() == 10000
-assert popped_data1.size == 50000
+popped_data1 = b.pop(10000)
+assert b.get_length() == 50
+assert popped_data1.size == 10000
+assert b._head == 10000
+assert b._tail == 10050
 
-b.push(np.ones(60000))
-assert b.get_length() == 70000
+b.push(np.ones(10050))
+assert b.get_length() == 10100
+assert b._head == 10000
+assert b._tail == 100
 
-popped_data2 = b.pop(50000)
-assert b.get_length() == 20000
-assert popped_data1.size == 50000
+popped_data2 = b.pop(10000)
+assert b.get_length() == 100
+print(f'Actual size is {popped_data2.size}')
+assert popped_data2.size == 10000
+assert b._head == 0
+assert b._tail == 100
